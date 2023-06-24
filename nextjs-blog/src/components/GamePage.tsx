afe17166401fc43b38dc4f670f2dbc759ece72b5
Pedro Santana <pedro.ferraz01@gmail.com>
2023-06-24 14:58:23 -0300

    style:responsividade

--- nextjs-blog/src/components/GamePage.tsx
+++ nextjs-blog/src/components/GamePage.tsx
@@ -40,10 +40,10 @@ const HomePage = (props: Props) => {
               {props.title}
             </h1>
           </div>
-          <div className="mb-2 ml-auto mr-4 w-96">
+          <div className="mb-2 ml-auto mr-4 w-72">
             <form onSubmit={handleSubmit}>
               <input
-                className="h-10 w-52 border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:w-full sm:text-xs md:w-full lg:w-full xl:w-full 2xl:w-full"
+                className="h-10 w-full border border-gray-300 bg-gray-50 p-2 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:w-full sm:text-xs md:w-full lg:w-full xl:w-full 2xl:w-full"
                 type="text"
                 placeholder="Search for title name "
                 onChange={(ev) => setSearch(ev.target.value)}
