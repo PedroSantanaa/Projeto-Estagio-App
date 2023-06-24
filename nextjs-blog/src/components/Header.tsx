133a9ec1094728e9ae0e83bdcb7493caa8881183
Pedro Santana <pedro.ferraz01@gmail.com>
2023-06-24 19:57:12 -0300

    style: finishing visual

--- nextjs-blog/src/components/Header.tsx
+++ nextjs-blog/src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = (props: Props) => {
       <h1 className="mb-4 mt-4 text-center font-serif text-4xl font-extrabold leading-none tracking-tight text-moon dark:text-moon md:text-5xl lg:text-6xl">
         {props.title}
       </h1>
-      <p className="text-center text-base md:text-base lg:text-lg xl:text-xl 2xl:text-xl">
+      <p className="text-center text-base text-black md:text-base lg:text-lg xl:text-xl 2xl:text-xl">
         {props.desc}
       </p>
     </div>
